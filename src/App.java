75d76e8f87dd12a1241256e058d8525c6a3b0db1
Jesiel Viana <jesielviana@proton.me>
2023-10-31 14:05:18 -0300

    Feedback sobre a atividade

--- src/App.java
+++ src/App.java
@@ -1,87 +1,42 @@
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         Cliente Carlos = new Cliente("Carlos", "123.231.432-12", LocalDate.of(2020, 03, 12),
                 new Endereco("240", "Davi Xereta", "Portelinha", "São Paulo", "SP"));
-        ContaCorrente contaCorrenteA = new ContaCorrente("20212", "3630", 1000, 0, Carlos, new NotificacaoEmail(), 3);
-        ContaPoupanca contaPoupancaB = new ContaPoupanca("20213", "3630", 1000, Carlos, new NotificacaoSMS());
+        ContaCorrente contaCorrenteA =
+                new ContaCorrente("20212", "3630", 1000, 0, Carlos, new NotificacaoEmail(), 3);
+        ContaPoupanca contaPoupancaB =
+                new ContaPoupanca("20213", "3630", 1000, Carlos, new NotificacaoSMS());
         Cliente Victor = new Cliente("Victor", "212.540.300-11", LocalDate.of(2020, 02, 13),
                 new Endereco("544", "Santo Antonio", "Centro", "Salvador", "BA"));
-        ContaCorrente contaCorrenteC = new ContaCorrente("20214", "3630", 1000, 1000, Victor, new NotificacaoSMS(), 2);
-        ContaPoupanca contaPoupancaD = new ContaPoupanca("20215", "3630", 1000, Victor, new NotificacaoEmail());
-        List<Cliente> clientes = new ArrayList<>();
-        clientes.add(Carlos);
-        clientes.add(Victor);
+        ContaCorrente contaCorrenteC =
+                new ContaCorrente("20214", "3630", 1000, 1000, Victor, new NotificacaoSMS(), 2);
+        ContaPoupanca contaPoupancaD =
+                new ContaPoupanca("20215", "3630", 1000, Victor, new NotificacaoEmail());
 
-        List<Conta> contas = new ArrayList<>();
-        contas.add(contaCorrenteA);
-        contas.add(contaPoupancaB);
-        contas.add(contaCorrenteC);
-        contas.add(contaPoupancaD);
 
-        for (int i = 0; i < clientes.size(); i++) {
-            Cliente cliente = clientes.get(i);
-
-            System.out.println("INFORMAÇÕES SOBRE O CLIENTE");
-            System.out.println("Nome: " + cliente.getNome());
-            System.out.println("CPF: " + cliente.getCPF());
-            System.out.println("Data de Nascimento: " + cliente.getDataNascimento());
-            System.out.println("Endereço: " + cliente.getEndereco().getRua() + ", " + cliente.getEndereco().getNumero()
-                    + ", " + cliente.getEndereco().getBairro() + ", " + cliente.getEndereco().getCidade() + ", "
-                    + cliente.getEndereco().getEstado());
-            System.out.println("*********************************");
-            System.out.println("*********************************");
-            for (int j = 0; j < contas.size(); j++) {
-                Conta conta = contas.get(j);
-                if (conta.getCliente().getCPF().equals(cliente.getCPF())) {
-                    if (conta instanceof ContaCorrente) {
-                        System.out.println("INFORMAÇÕES SOBRE A CONTA (CORRENTE) DO CLIENTE");
-                    } else if (conta instanceof ContaPoupanca) {
-                        System.out.println("INFORMAÇÕES SOBRE A CONTA (POUPANÇA) DO CLIENTE");
-                    }
-
-                    System.out.println("Número da conta: " + conta.getNumconta());
-                    System.out.println("Agência: " + conta.getAgencia());
-                    System.out.println("Saldo: " + conta.getSaldo());
-                    System.out.println("*********************************");
-                }
-            }
-
-        }
-
-        contaCorrenteA.depositar(80); // DEPOSITO CORRENTE
+        contaCorrenteA.depositar(100);
+        System.out.println(contaCorrenteA.getSaldo());
+        contaCorrenteA.sacar(100);
+        System.out.println(contaCorrenteA.getSaldo());
+        contaCorrenteA.transferir(contaCorrenteC, 100);
+        contaCorrenteA.transferir(contaCorrenteC, 100);
+        contaCorrenteA.transferir(contaCorrenteC, 100);
+        contaCorrenteA.transferir(contaCorrenteC, 100);
+        contaCorrenteA.transferir(contaCorrenteC, 100);
+        // esta transação não funcionou, mas deveria funcionar pegando o limite do cheque especial
+        contaCorrenteA.transferir(contaCorrenteC, 500);
+        contaCorrenteA.exibirTransacoes(); // deveria exibir o saldo atual no extrato
         System.out.println(contaCorrenteA.getSaldo());
 
-        contaCorrenteA.sacar(50); // SAQUE CORRENTE
-        System.out.println(contaCorrenteA.getSaldo());
-
-        contaCorrenteA.transferir(contaCorrenteC, 100); // TRANSFERENCIA CORRENTE
-        System.out.println(contaCorrenteA.getSaldo());
-
-        contaCorrenteA.transferir(contaCorrenteC, 75); // TRANSFERENCIA CORRENTE
-        System.out.println(contaCorrenteA.getSaldo());
-
-        contaCorrenteA.transferir(contaCorrenteC, 100); // TRANSFERENCIA CORRENTE COM DESCONTO
-        System.out.println(contaCorrenteA.getSaldo());
-
-
-        contaPoupancaB.depositar(80); // DEPOSITO POUPANÇA
         System.out.println(contaPoupancaB.getSaldo());
-
-        contaPoupancaB.sacar(30); // SAQUE POUPANÇA
+        contaPoupancaB.depositar(100);
         System.out.println(contaPoupancaB.getSaldo());
-
-                contaPoupancaB.transferir(contaPoupancaD, 95); // TRANSFERENCOA POUPANÇA
-        System.out.println(contaPoupancaD.getSaldo());
-
-        contaCorrenteA.exibirTransacoes();
-        contaPoupancaB.exibirTransacoes();
-
-        
+        contaPoupancaB.sacar(100);
+        System.out.println(contaPoupancaB.getSaldo());
+        contaPoupancaB.getTransacoes();
     }
 
 }
--- src/Conta.java
+++ src/Conta.java
@@ -3,82 +3,82 @@ import java.util.List;
 
 
 abstract class Conta {
-        protected final String Numconta;
-        protected final String Agencia;
-        protected Cliente cliente;
-        protected double saldo;
-        protected Notificacao notificacao;
-        private List<Transacao> transacoes;
+    protected final String Numconta;
+    protected final String Agencia;
+    protected Cliente cliente;
+    protected double saldo;
+    protected Notificacao notificacao;
+    private List<Transacao> transacoes;
 
-        public Conta(String Numconta, String Agencia, double saldo, Cliente cliente, Notificacao notificacao) {
-            this.Numconta = Numconta;
-            this.Agencia = Agencia;
-            this.saldo = saldo;
-            this.cliente = cliente;
-            this.notificacao = notificacao;
-            this.transacoes = new ArrayList<Transacao>();
-        }
-
-        public abstract void depositar(double valor);
-
-        public abstract void sacar(double valor);
-    
-        public abstract void transferir(Conta contaDestino, double valor);
-
-    
-
-        public String getNumconta() {
-            return Numconta;
-        }
-
-        public String getAgencia(){
-            return Agencia;
-        }
-    
-        public double getSaldo() {
-            return saldo;
-        }
-        public Cliente getCliente(){
-            return cliente;
-        }
-
-        public void setCliente(Cliente cliente){
-            this.cliente = cliente;
-        }
-        public void setSaldo(double saldo){
-    this.saldo = saldo;
- }
-public void setNotificacao(Notificacao notificacao){
-    this.notificacao = notificacao;
-}
-
-public Notificacao getNotificacao() {
-    return notificacao;
-}
-public List<Transacao> getTransacoes() {
-    return transacoes;
-}
-
-public void exibirTransacoes() {
-    System.out.println("\n***** Extrato de Transacoes da conta " + getNumconta() + " *****");
-    for (Transacao conta : transacoes) {
-        System.out.println(
-                "Tipo ->" + conta.getDescricao() +
-                        "\nValor -> R$" + conta.getValor() +
-                        "\nData ->" + conta.getData());
-        System.out.println("\n************");
+    public Conta(String Numconta, String Agencia, double saldo, Cliente cliente,
+            Notificacao notificacao) {
+        this.Numconta = Numconta;
+        this.Agencia = Agencia;
+        this.saldo = saldo;
+        this.cliente = cliente;
+        this.notificacao = notificacao;
+        this.transacoes = new ArrayList<Transacao>();
     }
-}
+
+    public abstract void depositar(double valor);
+
+    public abstract void sacar(double valor);
+
+    public abstract void transferir(Conta contaDestino, double valor);
 
 
-                @Override
+
+    public String getNumconta() {
+        return Numconta;
+    }
+
+    public String getAgencia() {
+        return Agencia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setNotificacao(Notificacao notificacao) {
+        this.notificacao = notificacao;
+    }
+
+    public Notificacao getNotificacao() {
+        return notificacao;
+    }
+
+    public List<Transacao> getTransacoes() {
+        return transacoes;
+    }
+
+    public void exibirTransacoes() {
+        System.out.println("\n***** Extrato de Transacoes da conta " + getNumconta() + " *****");
+        for (Transacao conta : transacoes) {
+            System.out.println("Tipo ->" + conta.getDescricao() + "\nValor -> R$" + conta.getValor()
+                    + "\nData ->" + conta.getData());
+            System.out.println("\n************");
+        }
+    }
+
+
+    @Override
     public String toString() {
-        return "Conta{" +
-                "numeroConta='" + Numconta + '\'' +
-                ", agencia='" + Agencia + '\'' +
-                ", saldo=" + saldo +
-                '}';
+        return "Conta{" + "numeroConta='" + Numconta + '\'' + ", agencia='" + Agencia + '\''
+                + ", saldo=" + saldo + '}';
     }
 
-    }
-    
+}
+
